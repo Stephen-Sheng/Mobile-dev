dcb5cb40bbfd7e0e3cf5d6eeca2eab4e5bae2ff9
YutongSheng <544518449@qq.com>
2022-04-24 16:32:20 +0100

    share viewModel between fragments

--- app/src/main/java/com/example/mobile_dev/LoginPage.kt
+++ app/src/main/java/com/example/mobile_dev/LoginPage.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -22,7 +23,7 @@ class LoginPage : Fragment() {
     }
 
     // delegate by viewModels, to prevent lose the state of view model
-    private val viewModel: ViewModel by viewModels()
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/mobile_dev/ProjectListPageFragment.kt
+++ app/src/main/java/com/example/mobile_dev/ProjectListPageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mobile_dev
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,7 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.mobile_dev.placeholder.PlaceholderContent
+import kotlin.math.log
 
 /**
  * A fragment representing a list of Items.
@@ -16,10 +19,11 @@ import com.example.mobile_dev.placeholder.PlaceholderContent
 class ProjectListPageFragment : Fragment() {
 
     private var columnCount = 1
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("Nickname",viewModel.nickname.value.toString())
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
