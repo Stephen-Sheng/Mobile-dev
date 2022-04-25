742d0e4c624ee529f376459dc1efb6baad36c024
YutongSheng <544518449@qq.com>
2022-04-26 00:02:24 +0100

    add bottom menu

--- app/src/main/java/com/example/mobile_dev/ProjectListPageFragment.kt
+++ app/src/main/java/com/example/mobile_dev/ProjectListPageFragment.kt
@@ -5,19 +5,21 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import com.example.mobile_dev.databinding.FragmentProjectListBinding
 import com.example.mobile_dev.placeholder.PlaceholderContent
-import kotlin.math.log
 
 /**
  * A fragment representing a list of Items.
  */
 class ProjectListPageFragment : Fragment() {
 
+    private var _binding: FragmentProjectListBinding? = null
+    private val binding get() = _binding!!
     private var columnCount = 1
     private val viewModel: ViewModel by activityViewModels()
 
@@ -33,19 +35,35 @@ class ProjectListPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_project_list, container, false)
+        _binding = FragmentProjectListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.list.adapter = MyProjectListPageRecyclerViewAdapter(PlaceholderContent.ITEMS)
+        binding.list.layoutManager = when {
                     columnCount <= 1 -> LinearLayoutManager(context)
                     else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyProjectListPageRecyclerViewAdapter(PlaceholderContent.ITEMS)
-            }
         }
-        return view
+        val duration = Toast.LENGTH_SHORT
+        binding.managerMenu.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.action_search ->{
+                    val text1 = "click 1"
+                    Toast.makeText(context, text1, duration).show()
+                }
+                R.id.action_settings ->{
+                    val text2 = "click 2"
+                    Toast.makeText(context, text2, duration).show()
+                }
+                R.id.action_navigation ->{
+                    val text3 = "click 3"
+                    Toast.makeText(context, text3, duration).show()
+                }
+            }
+            true
+        }
     }
 
     companion object {
